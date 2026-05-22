2867753d2add5c1b956274972ec342688852d7f4
Matheus Vicente <viceus.dev@gmail.com>
2026-05-21 22:21:27 -0300

    feat-(Python):Integer_Average|Square_Area

---
+++ Python/integer_average.py
@@ -0,0 +1,5 @@
+valor_a = int(input())
+valor_b = int(input())
+soma = valor_a + valor_b
+soma = int(soma / 2)
+print(soma)
---
+++ Python/square_area.py
@@ -0,0 +1,3 @@
+lado_quadrado = int(input())
+area_quadrado = lado_quadrado * lado_quadrado
+print(area_quadrado)
